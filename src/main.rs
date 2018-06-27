338540144e75b6badee4751a87c22f7cd450d75e
Armando Perez <armando@roastery.com>
2018-06-26 17:56:34 -0700

    Use another channel to report back status and progress

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 extern crate clap;
 extern crate colored;
-#[macro_use]
 extern crate crossbeam_channel;
 #[macro_use]
 extern crate lazy_static;
@@ -22,11 +21,16 @@ use walkdir::WalkDir;
 
 mod cli_args;
 
+#[derive(Debug)]
+enum Status {
+    Success(String),
+    Error(String),
+    Delta(u64),
+}
+
 fn start_threads(
     db_file_receiver: channel::Receiver<SQLiteFile>,
-    status_sender: channel::Sender<String>,
-    error_sender: channel::Sender<String>,
-    delta_sender: channel::Sender<u64>,
+    status_sender: channel::Sender<Status>,
 ) -> Vec<thread::JoinHandle<()>> {
     let cpu_count = num_cpus::get();
     let mut handles: Vec<thread::JoinHandle<_>> = Vec::with_capacity(cpu_count);
@@ -34,25 +38,23 @@ fn start_threads(
     for _ in 0..cpu_count {
         let db_files = db_file_receiver.clone();
         let status = status_sender.clone();
-        let error = error_sender.clone();
-        let delta = delta_sender.clone();
 
         handles.push(thread::spawn(move || {
             for db_file in db_files {
                 match db_file.vacuum() {
                     Ok(result) => {
-                        let size_delta = result.delta();
+                        let delta = result.delta();
 
-                        delta.send(size_delta);
-                        status.send(format!(
+                        status.send(Status::Delta(delta));
+                        status.send(Status::Success(format!(
                             "{} {} {}",
                             "Found".bold().green(),
                             db_file.path().to_str().unwrap_or("?").white(),
-                            format_size(size_delta as f64).yellow().bold(),
-                        ));
+                            format_size(delta as f64).yellow().bold(),
+                        )));
                     }
                     Err(err) => {
-                        error.send(format!("{:?}", err).red().to_string());
+                        status.send(Status::Error(format!("{:?}", err).red().to_string()));
                     }
                 };
             }
@@ -89,11 +91,9 @@ fn main() {
         });
 
     let (file_sender, file_receiver): ChannelAPI<SQLiteFile> = channel::unbounded();
-    let (status_sender, status_receiver): ChannelAPI<String> = channel::unbounded();
-    let (error_sender, error_receiver): ChannelAPI<String> = channel::unbounded();
-    let (delta_sender, delta_receiver): ChannelAPI<u64> = channel::unbounded();
+    let (status_sender, status_receiver): ChannelAPI<Status> = channel::unbounded();
 
-    let thread_handles = start_threads(file_receiver, status_sender, error_sender, delta_sender);
+    let thread_handles = start_threads(file_receiver, status_sender);
 
     for mut db_file in items {
         file_sender.send(db_file);
@@ -102,47 +102,16 @@ fn main() {
     drop(file_sender);
     let mut total_delta: u64 = 0;
 
-    loop {
-        // select! {
-        //     recv(delta_receiver, msg) => {
-        //         if let Some(delta) = msg {
-        //             total_delta += delta;
-        //         }
-        //     },
-        //     recv(status_receiver, msg) => {
-        //         if let Some(status) = msg {
-        //             println!("{}", status);
-        //         }
-        //     },
-        //     recv(error_receiver, msg) => {
-        //         if let Some(error) = msg {
-        //             eprintln!("{}", error);
-        //         }
-        //      },
-
-        //     default => { break; },
-        // }
-
-        let delta_msg = delta_receiver.recv();
-        let status_msg = status_receiver.recv();
-        let error_msg = error_receiver.recv();
-
-        match (&delta_msg, &status_msg, &error_msg) {
-            (None, None, None) => {
-                break;
-            },
-            _ => {
-                if let Some(delta) = delta_msg {
-                    total_delta += delta;
-                }
-
-                if let Some(status) = status_msg {
-                    println!("{}", status);
-                }
-
-                if let Some(error) = error_msg {
-                    eprintln!("{}", error);
-                }
+    for status in status_receiver {
+        match status {
+            Status::Success(msg) => {
+                println!("{}", msg);
+            }
+            Status::Error(msg) => {
+                eprintln!("{}", msg);
+            }
+            Status::Delta(delta) => {
+                total_delta += delta;
             }
         }
     }
